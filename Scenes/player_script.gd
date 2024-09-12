326463592ab2a690d01fb01d866e418cfc549fc4
Lucas_Lanto <lucaslantieri1997@gmail.com>
2024-09-12 11:28:23 -0300

    new changes for player

--- Scenes/player_script.gd
+++ Scenes/player_script.gd
@@ -1,85 +1,58 @@
 extends CharacterBody2D
 
-@onready var win_menu = $Player/Camera2D/Control
-
-#signal healthChanged
-
-var win_bar = false
-
-#export de variables para poder usarlas aparte 
+# Exportar variables para usarlas en el editor
 @export var speed = 300
-@export var gravity = 100
-@export var jump_force = 650	
-
+@export var jump_force = -400
 @export var maxHealth = 30
+
+#@onready var Bala = load("res://escenas/bala.tscn")
 @onready var currentHealth: int = maxHealth
 
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var isHurt: bool = false
 
 func _ready():
 	Global.playerBody = self
+	$AnimatedSprite2D.play("idle")
 	
-
 func _on_enemyDetector_body_entered(body: Node) -> void:
 	get_tree().reload_current_scene()
 
-#funcion para setear movimiento del personaje
-func _physics_process(delta):
-	if not is_on_floor():
-		velocity += get_gravity() * delta	
-		
-	#seteo variable de salto
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = -jump_force
-		
-	#var direction = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-
-	#seteo variable de Input para mover el personaje en R y L
-	var h_direction = Input.get_axis("move_left", "move_right")
-	
-		#aplico velocidad a X e Y para que pueda moverse
-	velocity.x = speed * h_direction
-	
-func _process(delta: float) -> void:
-	var right = Input.is_action_pressed("move_right")
-	var left = Input.is_action_pressed("move_left")
-	var space = Input.is_action_pressed("jump")
-	var shoot_gun = Input.is_action_pressed("shoot")
-	
-	
-	#seteo variables de animacion
-	if right and velocity.x != 0 and !space:
-		$AnimatedSprite2D.play("move")
-	elif left and velocity.x != 0 and !space:
-		$AnimatedSprite2D.play("move_L")
-		$AnimatedSprite2D.stop()
-	elif space and velocity.x != 0:
-		$AnimatedSprite2D.play("saltar")
-	elif shoot_gun and velocity.x != 0:
-		$AnimatedSprite2D.play("shoot_guns")
-	elif velocity.x == 0:
-		$AnimatedSprite2D.play("idle")
-		
-
-#func hurtByEnemy(area):
-#	currentHealth -= 10
-#	if currentHealth < 0:
-#		currentHealth = maxHealth
-		
-#	isHurt = true
-#	healthChanged.emit()
-	
-	
+# Función para manejar la física del personaje
+func _physics_process(delta: float) -> void:
 	move_and_slide()
-	
-func _wingame():
-	if !win_bar:		
-		win_menu.hide()
-		Engine.time_scale = 1
+	# Aplicar gravedad si no está en el suelo
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	var direction = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+
+	if direction != 0:
+		velocity.x = direction * speed
+		# Si está en el suelo, reproducir animación de movimiento
+		if is_on_floor():
+			$AnimatedSprite2D.play("move")
+		# Si no está en el suelo, reproducir la animación de salto
+		else:
+			$AnimatedSprite2D.play("saltar")
+		$AnimatedSprite2D.flip_h = direction < 0
 	else:
-		win_menu.show()
-		Engine.time_scale = 0
+		velocity.x = move_toward(velocity.x, 0, speed)
+		# Solo reproducir la animación de idle si está en el suelo y no hay dirección de movimiento
+		if is_on_floor():
+			$AnimatedSprite2D.play("idle")
+	
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y = jump_force
 
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	_wingame()
+	elif not is_on_floor():
+		$AnimatedSprite2D.play("saltar")
+	
+	#instancia el doble salto 
+	#if not is_on_floor() and Input.is_action_just_pressed("ui_accept"):
+	#	velocity.y = jump2  #creando la constante o varia
+	
+	#shoot
+	var shoot_gun = Input.is_action_pressed("shoot")
+	if Input.is_action_pressed("shoot"):
+		$AnimatedSprite2D.play("shoot_guns")
